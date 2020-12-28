dd27eeab9f40c2e8c03ec096e850b6e4a988d220
LVaras <lvaras@aligare.cl>
2020-12-28 14:51:44 -0300

    ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,4 +1,26 @@
 def call(){
 
+pipeline{
+	agent any
+	parameters{choice(name: 'buildtool', choices:['gradle','maven'], description:'Parametros de herramientas a elegir')}
+	stages{
+			stage('Pipeline'){
+				steps{
+					script{
+					if(params.buildtool == 'gradle' ){
+					gradle.call();
+					}else{
+					maven.call();
+					}
+					}
+				}
+				
+			}
+	}
+
+}
+
+
+	
 }
 return this;
