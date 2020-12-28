0adf801675518f784f51256e54ed70712d0714da
LVaras <lvaras@aligare.cl>
2020-12-28 14:40:15 -0300

    se agregan archivos .groovy

---
+++ vars/gradle.groovy
@@ -0,0 +1,51 @@
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
+
+def call(){
+  
+	stage('Build & Test'){
+		env.Tarea='Build & Test'
+		bat './gradlew clean build'
+		
+		}
+		
+	stage('Sonar'){
+		env.Tarea='Sonar'
+					def scannerHome = tool 'sonar';
+					withSonarQubeEnv('sonar') {
+						bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+					}
+		}
+	stage('Run'){
+			env.Tarea='Run'
+			bat 'start gradlew bootRun'
+			sleep 7
+		}
+	stage('Test'){
+		env.Tarea='Test'
+					bat "curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing"
+		}
+	stage('Nexus Upload'){
+		env.Tarea='Nexus Upload'
+						nexusArtifactUploader(
+						nexusVersion: 'nexus3',
+						protocol: 'http',
+						nexusUrl: 'http://localhost:8081/',
+						groupId: 'com.devopsusach2020',
+						version: '0.0.1',
+						repository: 'test-nexus',
+						credentialsId: 'nexus',
+						artifacts: [
+						[artifactId: 'DevOpsUsach2020',
+						classifier: '',
+						file: 'C:/Users/luisv/OneDrive/Taller11/ejemplo-gradle/build/DevOpsUsach2020-0.0.1.jar',
+						type: 'jar']
+						]
+						)
+		}
+}
+
+return this;
---
+++ vars/maven.groovy
@@ -0,0 +1,52 @@
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
+
+def call(){
+stage('Compile'){
+		env.TAREA='Compile'
+		bat './mvnw.cmd clean compile -e'
+		
+	}
+	 stage('Unit Test'){
+	 env.TAREA='Unit Test'
+		bat './mvnw.cmd clean test -e'
+		
+	}
+
+	 stage('Jar'){
+	 	 env.TAREA='Jar'
+		bat './mvnw.cmd clean package -e'
+		
+	}
+	stage('Sonar'){
+		env.TAREA='Sonar'
+		def scannerHome = tool 'sonar';
+		withSonarQubeEnv('sonar') {
+			bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+		}
+	}
+	stage('Nexus Upload'){
+		env.TAREA='Nexus Upload'
+		nexusArtifactUploader(
+		nexusVersion: 'nexus3',
+		protocol: 'http',
+		nexusUrl: 'http://localhost:8081/',
+		groupId: 'com.devopsusach2020',
+		version: '0.0.1',
+		repository: 'test-nexus',
+		credentialsId: 'nexus',
+		artifacts: [
+			[artifactId: 'DevOpsUsach2020',
+			classifier: '',
+			file: 'C:/Users/luisv/OneDrive/Taller11/ejemplo-gradle/build/DevOpsUsach2020-0.0.1.jar',
+			type: 'jar']
+		]
+		)
+	}
+
+}
+
+return this;
