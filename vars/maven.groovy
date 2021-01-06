feb493bbb6d42683a3635cb60e599717a4ff1b3d
LVaras <lvaras@aligare.cl>
2021-01-06 20:24:30 -0300

    cambio rutas maven

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -48,12 +48,12 @@ def Sonar(){
 
 def downloadNexus(){
     figlet "downloadNexus"
-    bat 'curl -X GET -u admin:Mortal2112 http://localhost:8081/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"'
+    bat 'curl -X GET -u admin:Mortal2112 http://localhost:8081/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1-develop.jar -O"'
 }
 
 def runDownload() {
     figlet 'runDownloadedJar'
-     bat 'start java -jar DevOpsUsach2020-0.0.1.jar' 
+     bat 'start java -jar DevOpsUsach2020-0.0.1-develop.jar' 
     sleep 7
 }
 def rest() {
