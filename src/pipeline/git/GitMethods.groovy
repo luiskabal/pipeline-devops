abb3a498d313c2d561ef4b02aa5ff55946225bd3
LVaras <lvaras@aligare.cl>
2021-01-09 20:00:53 -0300

    bat 'git pull'

--- src/pipeline/git/GitMethods.groovy
+++ src/pipeline/git/GitMethods.groovy
@@ -4,8 +4,9 @@ def call(){
 	
 }
 def checkIfBranchExists(releaseBranchName){
-	println releaseBranchName
-    def output = bat (script:"git pull; git ls-remote --heads origin "+releaseBranchName, returnStdout: true)
+	println "PS I LOVE YOU:" +releaseBranchName
+	bat 'git pull'
+    def output = bat (script:"git ls-remote --heads origin "+releaseBranchName, returnStdout: true)
 	println output
 	respuesta= (!output?.trim())?false:true
 	return respuesta
