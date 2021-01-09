09f21b87d1ae28e3c6a38923302d1280700340ec
LVaras <lvaras@aligare.cl>
2021-01-09 19:02:12 -0300

    def output = bat (script:"git log origin/"+releaseBranchName+"..origin/"+currentBranch, returnStdout: true)

--- src/pipeline/git/GitMethods.groovy
+++ src/pipeline/git/GitMethods.groovy
@@ -9,7 +9,7 @@ def checkIfBrandExists(releaseBranchName){
 def checkIfBrandUpdated(currentBranch,releaseBranchName){
 
 	println("BEGINS  checkIfBrandUpdated(): "+ "git log origin/"+releaseBranchName+"..origin/"+currentBranch)
-	def output = bat ("git log origin/"+releaseBranchName+"..origin/"+currentBranch, returnStdout: true)
+	def output = bat (script:"git log origin/"+releaseBranchName+"..origin/"+currentBranch, returnStdout: true)
 	println("BRAND UPDATED: "+ output)
 	//respuesta= (!output?.trim())?false:true
 
