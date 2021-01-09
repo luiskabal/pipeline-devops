d8961a501ff3955a0974e4e9d732b01b0c3e7c16
LVaras <lvaras@aligare.cl>
2021-01-09 20:04:07 -0300

    pull

--- src/pipeline/git/GitMethods.groovy
+++ src/pipeline/git/GitMethods.groovy
@@ -9,19 +9,22 @@ def checkIfBranchExists(releaseBranchName){
     def output = bat (script:"git ls-remote --heads origin "+releaseBranchName, returnStdout: true)
 	println output
 	respuesta= (!output?.trim())?false:true
+	println respuesta
 	return respuesta
 }
 def checkIfBranchUpdated(currentBranch,releaseBranchName){
-
+	bat 'git pull'
 	println("BEGINS  checkIfBranchUpdated(): "+ "git log origin/"+releaseBranchName+"..origin/"+currentBranch)
-	def output = bat (script:"git pull; git log origin/"+releaseBranchName+"..origin/"+currentBranch, returnStdout: true)
+	def output = bat (script:"git log origin/"+releaseBranchName+"..origin/"+currentBranch, returnStdout: true)
 	println("BRAND UPDATED: "+ output)
 	respuesta= (!output?.trim())?false:true
 
 	return respuesta ;
 }
 def deleteBranch(releaseBranchName){
-bat "git pull; git push origin --delete " + releaseBranchName
+	bat '''
+	git pull
+	git push origin --delete '''+ releaseBranchName
 }
 def createBranch(releaseBranchName,currentBranch){
 bat 
