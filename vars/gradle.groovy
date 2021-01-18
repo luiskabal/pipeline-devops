91c1636a497c8bf30946032574296ad9943bf2b0
LVaras <lvaras@aligare.cl>
2021-01-18 14:30:53 -0300

    env.NombreStage='buildAndTest'

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -23,7 +23,7 @@ def call(String type, String chosenStages, String jobName){
 }
 
 def buildAndTest() {
-    env.NombreStage="buildAndTest"
+    env.NombreStage='buildAndTest'
     figlet "buildAndTest"
     bat './gradlew clean build'
     println(" Ejecutado buildAndTest"+ env.NombreStage)
